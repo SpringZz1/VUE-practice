02abe5a24b5cd0a5ce53e90c27dd318e09955ee1
SpringZz <wang90944@gmail.com>
2022-08-11 14:16:03 +0800

    todolist项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import navHeader from '@/components/navHeader/index.vue'
+import navMain from '@/components/navMain/index.vue'
+import navFooter from '@/components/navFooter/index.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    navHeader,
+    navMain,
+    navFooter
+  },
+  setup() {}
+})
+</script>
+
+<style scoped lang="scss">
+</style>
